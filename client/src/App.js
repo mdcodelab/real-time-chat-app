efa1a0c0a9389f0674aa2ad50b4d85788789217e
miha11-hash <d_mihaela@msn.com>
2024-01-07 05:13:05 +0200

    connect react & socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:4000");
 
 function App() {
   return (
--- server/index.js
+++ server/index.js
@@ -1,32 +1,26 @@
-const express=require("express");
+const express = require("express");
 const app = express();
-
 const http = require("http");
-
 const cors = require("cors");
-const { start } = require("repl");
+const { Server } = require("socket.io");
 
 app.use(cors());
-
-const {Server} = require("socket.io");
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST", ]
-    }
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log(socket.id);
+  console.log(`User connected: ${socket.id}`);
 
-    io.on("disconnect", () => {
-        console.log("User disconnected", socket.id);
-    });
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
 });
 
-server.listen(4000, () => console.log("server is listening ... "))
-
-
+server.listen(4000, () => console.log("Server is listening on port 4000"));
