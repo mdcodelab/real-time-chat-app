ccb41db1ed2a84b1f70027709b46230c23ddc0b9
miha11-hash <d_mihaela@msn.com>
2024-01-07 13:06:46 +0200

    installed concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
+import React from "react";
 import './App.css';
+import styled from "styled-components";
+import { IoSendOutline } from "react-icons/io5";
 import io from "socket.io-client";
 
 const socket = io.connect("http://localhost:4000");
 
 function App() {
+  const[username, setUsername]=React.useState("");
+  const [room, setRoom] = React.useState("");
+
+  function joinRoom () {
+    if(username !== "" && room !==""){
+      socket.emit("join_room", room);
+      
+    }
+  }
+
+
   return (
-    <div className="App">
-      hello
-    </div>
+    <Wrapper className="App">
+      <h3>How can I help?</h3>
+      <input type="text" placeholder="Message ..." onChange={(e)=> setUsername(e.target.value)}></input>
+      <input type="text" placeholder="Room ID..." onChange={(e)=> setRoom(e.target.value)}></input>
+      <button onClick={joinRoom}><IoSendOutline></IoSendOutline></button>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+
+`;
 
 export default App;
